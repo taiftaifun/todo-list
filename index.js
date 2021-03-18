7e9f14ac084562e4a7d489933ee7467a06fdd511
Mike <michalstefaniak@yahoo.com>
2021-03-18 08:36:44 +0100

    Added the functionality of adding new projects

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const projects = [];
 
 const DOM = (() => {
+    const projectsDiv = document.querySelector("#projects");
     const newProjectBtn = document.querySelector("#new-project-btn");
     const projectWizard = document.querySelector("#new-project-form");
     const projectWizardCancelBtn = document.querySelector("#cancel-project-btn");
     const projectNameInput = document.querySelector("#project-name-input");
+    projectNameInput.addEventListener("keyup", enterAdd)
     const projectWizardAddBtn = document.querySelector("#add-project-btn");
     newProjectBtn.addEventListener("click", showProjectWizard);
     projectWizardCancelBtn.addEventListener("click", hideProjectWizard);
@@ -13,32 +15,46 @@ const DOM = (() => {
     function showProjectWizard() {
             projectWizard.style.display = "block";
         }
+
     function hideProjectWizard() {
         projectWizard.style.display = "none";
+        projectNameInput.value = "";
     }
-    function addProject() {
-        if(projectNameInput.value != "") {
-            projects.push(Project(projectNameInput.value));
-            hideProjectWizard();
+
+    function enterAdd(e) {
+        if(e.keyCode === 13) {
+            e.preventDefault();
+            projectWizardAddBtn.click();
         }
     }
-})();
 
-const Task = (taskName, dueDate, priority, taskDetails, projectName) => {
-    //
-}
+    function addProject() {
+        if(projectNameInput.value.trim() != "") {
+            console.log(projectNameInput.value);
+            let newProject = Project(projectNameInput.value);
+            projects.push(newProject);
+            hideProjectWizard();
+            renderProject(newProject);
+            console.log(projects);
+        }
+    }
+
+    function removeProject(e) {
+        //
+    }
+
+    function renderProject(project) {
+        let projectLi = document.createElement("li");
+        projectLi.className = "project";
+        projectLi.id = project.name;
+        projectLi.textContent = project.name;
+        projectsDiv.appendChild(projectLi);
+        }
+})();
 
 const Project = (name) => {
     let tasks = [];
-    addTask = (taskName) => {
-        tasks.push(Task);
+    return {name, tasks};
     }
-
-    removeTask = (taskName) => {
-        tasks = tasks.filter(task.name != taskName);
-    }
-
-    return {tasks};
-}
 
 let defaultProject = Project("Your First");
